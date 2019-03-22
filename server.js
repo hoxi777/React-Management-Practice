bf05165d34f10927fffc804ed558d85d86e03b31
hoxi777 <jtho1015@gmail.com>
2019-03-22 18:23:05 +0900

    Create Customer Database Table & Node.js Database Connection Settiongs

--- server.js
+++ server.js
@@ -2,39 +2,33 @@ const express = require('express');
 const bodyParser =require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = request('fs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host : conf.host,
+  user : conf.user,
+  password : conf.password,
+  port : conf.port,
+  database : conf.database
+});
+
+connection.connect();
 
 //배열 데이터를 제이슨으로 
 app.get('/api/customers', (req,res) => {
-    res.send([{
-        "id" : 1,
-        "image" : "https://placeimg.com/64/64/1",
-        "name" : "정태현",
-        "birth" : 911015,
-        "gender" : "남자",
-        "job" : "대학생"
-      },
-      {
-        "id" : 2,
-        "image" : "https://placeimg.com/64/64/2",
-        "name" : "나루토",
-        "birth" : 20011212,
-        "gender" : "남자",
-        "job" : "프로그래머"
-      },
-      {
-        "id" : 3,
-        "image" : "https://placeimg.com/64/64/any",
-        "name" : "루피",
-        "birth" : 981029,
-        "gender" : "남자",
-        "job" : "의사"
-      }
-    ])
-
+      connect.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+          res.send({
+        } 
+      );    
 });
 
 app.listen(port, () => console.log(`Listen on port  ${port}`));
