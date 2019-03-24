3a6ccd5228c3d786993f8cc28dd3ae4713908ec7
hoxi777 <jtho1015@gmail.com>
2019-03-24 14:45:24 +0900

    Add Customer Delete Module

--- client/src/App.js
+++ client/src/App.js
@@ -69,6 +69,7 @@ class App extends Component {
   _renderCustomers = () => {
     const customers = this.state.customers.map((customers) => {
       return <Customer
+        stateRefresh={this.stateRefresh}
         id={customers.id}
         key={customers.id}
         image={customers.image}
@@ -102,6 +103,7 @@ class App extends Component {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ connection.connect();
 //배열 데이터를 제이슨으로 
 app.get('/api/customers', (req,res) => {
       connection.query(
-        "SELECT * FROM CUSTOMER",
+        "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
         (err, rows, fields) => {
           res.send(rows);
         } 
@@ -37,7 +37,7 @@ app.get('/api/customers', (req,res) => {
 
 app.use('/image', express.static('./upload'));
 app.post('/api/customers',upload.single('image'), (req, res) => {
-  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
   let image = '/image/' + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
@@ -45,6 +45,17 @@ app.post('/api/customers',upload.single('image'), (req, res) => {
   let job = req.body.job;
   let params = [image, name, birthday, gender, job];
 
+
+  connection.query(sql, params,
+    (err, rows, fields) =>{
+      res.send(rows);
+     }
+    );
+});
+
+app.delete('/api/customers/:id', (req, res) =>{
+  let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+  let params = [req.params.id];
 
   connection.query(sql, params,
     (err, rows, fields) =>{
