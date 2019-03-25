32cf54add83f12ce2a880515fff3fd754cc11b98
hoxi777 <jtho1015@gmail.com>
2019-03-25 15:30:39 +0900

    Add Customer Search Module

--- client/src/App.js
+++ client/src/App.js
@@ -11,17 +11,89 @@ import TableCell from "@material-ui/core/TableCell";
 import { withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
 const styles = theme => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing.unit * 3,
-    overflowX: "auto",
+    minWidth : 1080
   },
-  table: {
-    minWidth: 1080
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: 'flex',
+    justifyContent: 'center'
   },
   progress: {
     margin: theme.spacing.unit * 2,
+  },
+  paper: {
+    marginLeft : 18,
+    marginRight : 18
+  },
+  TableHead: {
+    fontSize: '1.0rem'
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      }
+    }
   }
 
 })
@@ -35,15 +107,18 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      customers:'',
-      completed : 0
+      customers: '',
+      completed: 0,
+      searchKeyword: ''
+
     }
   }
 
   stateRefresh = () => {
     this.setState({
-      customers : '',
-      completed : 0
+      customers: '',
+      completed: 0,
+      searchKeyword: ''
     });
     this._getCustomers()
   }
@@ -55,7 +130,7 @@ class App extends Component {
 
   _getCustomers = () => {
     this._callApi()
-      .then(res => this.setState({ customers : res }))
+      .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err))
   }
 
@@ -89,27 +164,84 @@ class App extends Component {
 
   }
 
+  handleValueChange = (e) => {
+    let nextState ={};
+
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+
+  }
+
   render() {
+    const filteredComponents = (data) => {
+
+      data = data.filter((c) => {
+        return c.name.indexOf(this.state.searchKeyword) > -1;
+      });
+
+      return data.map((customers) => {
+        return <Customer
+        stateRefresh={this.stateRefresh}
+        id={customers.id}
+        key={customers.id}
+        image={customers.image}
+        name={customers.name}
+        birthday={customers.birthday}
+        gender={customers.gender}
+        job={customers.job}
+      />
+      });
+    }
     const { classes } = this.props;
+    const cellist = ["번호", "프로필", "이름", "생년월일", "성별", "직업", "설정"]; 
     return (
-      <div>
-        <Papaer className={classes.root}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+              고객관리 시스템
+          </Typography>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="검색하기"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                name="searchKeyword"
+                value={this.state.searchKeyword}
+                onChange={this.handleValueChange}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        <div className={classes.menu}>
+          <CustomerADD stateRefresh={this.stateRefresh} />
+
+        </div>
+
+
+        <Papaer className={classes.paper}>
           <Table className={classes.table}>
             <TableHead>
               <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-                <TableCell>설정</TableCell>
+                {cellist.map(c => {
+                  return <TableCell className={classes.TableHead} >{c}</TableCell>
+                }) }
               </TableRow>
             </TableHead>
             <TableBody>
 
 
-              {this.state.customers ? this._renderCustomers() :
+              {this.state.customers ? filteredComponents(this.state.customers) :
                 <TableRow>
                   <TableCell colSpan="6" align="center">
                     <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
@@ -120,7 +252,6 @@ class App extends Component {
             </TableBody>
           </Table>
         </Papaer>
-        <CustomerADD stateRefresh={this.stateRefresh}/>
       </div>
 
     );
