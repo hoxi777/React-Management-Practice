a590f646bc4075b8a41aefbce3e03b6c703521be
hoxi777 <jtho1015@gmail.com>
2019-03-25 14:27:27 +0900

    Add Materal Modal at Delete Button &  Customer Add

--- client/src/App.js
+++ client/src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
         key={customers.id}
         image={customers.image}
         name={customers.name}
-        birth={customers.birth}
+        birthday={customers.birthday}
         gender={customers.gender}
         job={customers.job}
       />
--- client/src/components/CustomerADD.js
+++ client/src/components/CustomerADD.js
@@ -1,41 +1,76 @@
 import React from 'react';
-import {post} from 'axios';
+import { post } from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { DialogActions } from '@material-ui/core';
 
-class CustomerAdd extends React.Component{
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+});
+
+
+class CustomerAdd extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            file : null,
-            userName : '',
-            birthday : '',
-            gender : '',
-            job : '',
-            fileName : ''
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
         }
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+
+    }
+
+    handleClose = () => {
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
+        });
     }
 
     handleFormSubmit = (e) => {
         e.preventDefault();
         this.addCustomer()
-        .then((response) => {
-            console.log(response.data);
-            this.props.stateRefresh();        
-        })
+            .then((response) => {
+                console.log(response.data);
+                this.props.stateRefresh();
+            })
 
         this.setState({
-            file : null,
-            userName : '',
-            birthday : '',
-            gender : '',
-            job : '',
-            fileName : ''
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
         });
     }
 
     handleFileChange = (e) => {
         this.setState({
-            file : e.target.files[0],
-            fileName : e.target.value
+            file: e.target.files[0],
+            fileName: e.target.value
         });
     }
 
@@ -54,8 +89,8 @@ class CustomerAdd extends React.Component{
         formData.append('gender', this.state.gender);
         formData.append('job', this.state.job);
         const config = {
-            headers : {
-                'content-type' : 'multipart/form.data'
+            headers: {
+                'content-type': 'multipart/form.data'
             }
         }
 
@@ -64,18 +99,36 @@ class CustomerAdd extends React.Component{
     }
 
     render() {
+        const { classes } = this.props;
         return (
-            <form onSubmit = {this.handleFormSubmit}>
-                <h1>고객추가</h1>
-                프로필이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /> <br/>
-                이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handeValueChange} /> <br/>
-                생년월일: <input type="text" name="birthday" vlaue={this.state.birthday}  onChange={this.handeValueChange} /> <br/>
-                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handeValueChange} /> <br/>
-                직업: <input type="text" name="job" value={this.state.job} onChange={this.handeValueChange} /> <br/>
-                <button type="submit">추가하기</button>
-            </form>
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    고객 추가하기
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>고객 추가</DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="rasied-button-file" type="file" file={this.state.file} vlaue={this.state.fileName} onChange={this.handleFileChange}/><br/>
+                        <label htmlFor="rasied-button-file">
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileName === "" ? "프로필 이미지 선택" :  this.state.fileName}
+                            </Button>
+                        </label>
+                        <br/>
+                        <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handeValueChange} /> <br />
+                        <TextField label="생년월일" type="text" name="birthday" vlaue={this.state.birthday} onChange={this.handeValueChange} /> <br />
+                        <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handeValueChange} /> <br />
+                        <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handeValueChange} /> <br />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary"onClick={this.handleFormSubmit}>추가</Button>
+                        <Button variant="outlined" color="primary"onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+
+                </Dialog>
+            </div>
         )
     }
 }
 
-export default CustomerAdd;
+export default withStyles(withStyles)(CustomerAdd);
